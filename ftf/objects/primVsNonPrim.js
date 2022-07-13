e7d24c023898a0e2a321ed7825afa47313358d74
Irfan-vit <65129024+Irfan-vit@users.noreply.github.com>
2022-07-13 04:59:57 -0700

    Update primVsNonPrim.js

--- ftf/objects/primVsNonPrim.js
+++ ftf/objects/primVsNonPrim.js
@@ -10,11 +10,18 @@ console.log(tst[0]);
 
 
 var prim = "sting"
+//here the value of prim is "string"
 
 function test(prim) {
     prim = "i'm a primitive DS";
     console.log(prim);
+    // in this function the value of prim is "i'm a primitive DS".
 }
 test(prim)
 
+prim = "now im changing my value by creating new memory block"
+// here the value of prim is changed and new memory for prim is created instead of modifying the initial one and the this value will be followed from hear-onwards but the old value will be followed till the above code. 
+
 console.log(prim);
+
+// But in case of Non-primitive as they are passed by refrence if the refrence is changed once then the value of such non-primitive variable is changed in the entire code and we might get different results if we run the code again.
